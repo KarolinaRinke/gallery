3f4f1dcc48c1bdbd7ced9af9c106cfb2e62a6a16
KarolinaRinke <106106228+KarolinaRinke@users.noreply.github.com>
2022-05-23 19:27:43 +0200

    first images test

--- script/dataloader.js
+++ script/dataloader.js
@@ -1,1 +1,20 @@
-imgData =[]
+imgData =[
+    {
+        "name": "sky",
+        "imgs": [
+            "Himmel_1080-1.JPG",
+            "Himmel_1080-6420.JPG",
+            "Himmel_1080-6422.JPG",
+            "Himmel_1080-6428.JPG",
+            "Himmel_1080-6429.JPG",
+            "Himmel_1080-6430.JPG",
+            "Himmel_1080-6433.JPG",
+            "Himmel_1080-6438.JPG",
+            "Himmel_1080-6439.JPG",
+            "Himmel_1080-6443.JPG",
+            "Himmel_1080-6444.JPG",
+            "Himmel_1080-6446.JPG",
+            "Himmel_1080-6449.JPG"
+        ]
+    }
+]
