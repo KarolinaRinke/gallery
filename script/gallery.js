46fd428874e4d3bf8a843d4729db3d03e7d08af0
KarolinaRinke <106106228+KarolinaRinke@users.noreply.github.com>
2022-05-23 19:32:26 +0200

    update push

--- script/gallery.js
+++ script/gallery.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('#header');
 const FolderConntainer = document.querySelector('#FolderConntainer');
 const backArrow = document.querySelector('#backArrow');
-
+const downladBTN = document.querySelector('#downladBTN');
 
 var foldPrefab="", imgPrefab="";
 fetch('./prefab/fold_prefab.html')
@@ -22,6 +22,8 @@ function loadDataIntoGallery(folder)
     if (folder==0) {loadFoldersIntoGallery();}
     header.innerHTML =  imgData[folder]["name"];
     backArrow.style.display = "block";
+    downladBTN.style.display = "block";
+    downladBTN.setAttribute("href", "./zip/"+imgData[folder]["name"]+".zip");
     FolderConntainer.innerHTML = "";
     imgData[folder]["imgs"].forEach(img=>{
         FolderConntainer.innerHTML += imgPrefab.replace(/NAME/g, "./data/" +  imgData[folder]["name"] + "/" +  img);
@@ -34,6 +36,7 @@ function loadFoldersIntoGallery()
     header.innerHTML = "Gallerieauswahl";
     FolderConntainer.innerHTML = "";
     backArrow.style.display = "none";
+    downladBTN.style.display = "none";
     var i = 0;
     imgData.forEach(folder=>{
         FolderConntainer.innerHTML += foldPrefab.replace(/NAME/g, folder.name).replace(/ID/g, i);
--- update_gallery.py
+++ update_gallery.py
@@ -1,7 +1,10 @@
 import os
 import json
 from glob import glob
+import shutil
 data = []
+for f in glob("./zip/*", recursive = False):
+    os.remove(f)
 folders = glob("./data/*", recursive = False)
 folders.sort(key=os.path.getmtime, reverse=True)
 i = 0
@@ -9,6 +12,7 @@ for folder in folders:
     data.append({"name": os.path.basename(folder), "imgs": []})  
     for file in glob(folder+"/*"):
         data[i]["imgs"].append(os.path.basename(file))
+    shutil.make_archive("./zip/"+os.path.basename(folder), 'zip', folder)
     i+=1
 out = "imgData =" + json.dumps(data, indent=4)
 with open("script/dataloader.js", "w") as f:
